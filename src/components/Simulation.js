e5c16c6e159cd4d024d69b200497d6bbf9f77991
McDevon <jussi.enroos@gmail.com>
2019-07-17 20:29:23 +0300

    Add simulation component

---
+++ src/components/Simulation.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+const Simulation = () => {
+    const [counter, setCounter] = useState(10)
+    const canvasElement = useRef(null)
+    const canvasStyle = {
+        border: ' 1px solid #aaa',
+    }
+
+    const decrease = () => () => {
+        const newValue = Math.max(counter - 1, 0)
+        setCounter(newValue)
+        canvasElement.current['counter'] = newValue
+    }
+    const increase = () => () => {
+        const newValue = Math.min(counter + 1, 20)
+        setCounter(newValue)
+        canvasElement.current['counter'] = newValue
+    }
+
+    const startHook = () => {
+        console.log('start hook')
+        canvasElement.current['counter'] = 10
+    }
+
+    useEffect(startHook, [])
+
+    return <div>
+        <canvas style={canvasStyle} ref={canvasElement} width="400" height="300" />
+        <div>
+            {counter}
+            <button onClick={increase()}>
+                +
+            </button>
+            <button onClick={decrease()}>
+                -
+            </button>
+        </div>
+    </div>
+}
+
+export default Simulation
